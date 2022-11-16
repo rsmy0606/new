564b0eea751954c620d908c2a30da21292effdac
rsmy0606 <muraliyovelrs@gmail.com>
2022-11-16 12:40:13 +0530

    commite first

---
+++ src/org/encapsulation/Practise.java
@@ -0,0 +1,57 @@
+package org.encapsulation;
+
+public class Practise extends sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Practise p= new Practise();
+		
+		p.setEmpId(1234);
+		p.setName("balu");
+		p.setSal(46.60f);
+		p.setAvg(12.35);
+		p.setRegno(9);
+		p.setAccount(12345678);
+		
+		
+		System.out.println("practise id:"+p.getEmpId());
+		System.out.println("practise name:"+p.getName());
+		System.out.println("practise salary:"+p.getSal());
+		System.out.println("practise average:"+p.getAvg());
+		System.out.println("practise account:"+p.getAccount());
+		System.out.println("practise registered no:"+p.getRegno());
+		
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private void setAccount(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setRegno(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+	}
+
+
---
+++ src/org/encapsulation/sample.java
@@ -0,0 +1,58 @@
+package org.encapsulation;
+
+public class sample {
+		
+	private	int empId;
+	private	String Name;
+	
+	private	float Sal;
+	private double Avg;
+	private short regno;
+	private long account;
+	{
+	
+}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public float getSal() {
+		return Sal;
+	}
+	public void setSal(float sal) {
+		Sal = sal;
+	}
+	public double getAvg() {
+		return Avg;
+	}
+	public void setAvg(double avg) {
+		Avg = avg;
+	}
+	public long getAccount() {
+		return account;
+	}
+	public void setAccount(long account) {
+		this.account = account;
+	}
+	public short getRegno() {
+		return regno;
+	}
+	public void setRegno(short regno) {
+		this.regno = regno;
+	}	
+	
+	
+	
+		
+ 
+	}
+
+
